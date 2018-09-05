4520c20c52905f4bd31cc5571572ea95ed1e5f56
Dmytro_Slobodenyuk <Dmytro_Slobodenyuk@epam.com>
2018-09-05 11:41:30 +0300

    Commment is added

--- src/main/java/com/slobodenyuk/vetclinic/configs/DatabaseConfig.java
+++ src/main/java/com/slobodenyuk/vetclinic/configs/DatabaseConfig.java
@@ -55,6 +55,7 @@ public class DatabaseConfig {
         sessionFactoryBean.setPackagesToScan(ENTITYMANAGER_PACKAGES_TO_SCAN);
         Properties hibernateProperties = new Properties();
         //hibernateProperties.put("hibernate.dialect", HIBERNATE_DIALECT);
+        // show sql statements in console
         hibernateProperties.put("hibernate.show_sql", HIBERNATE_SHOW_SQL);
         hibernateProperties.put("hibernate.hbm2ddl.auto", HIBERNATE_HBM2DDL_AUTO);
         sessionFactoryBean.setHibernateProperties(hibernateProperties);
